c518d4792d3e8ffbf0a00017bbcd58134672c1d4
Raphaël Valyi <rvalyi@gmail.com>
2013-09-02 04:53:04 +0200

    [CONTACT][TRAVIS] basic Travis-CI testing

---
+++ .travis_test_script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ../server
+
+./openerp-server --db_user=postgres --db_user=openerp --db_password=admin --db_host=localhost --stop-after-init --addons-path=../openerp-addons -i sale,purchase,stock,crm,project -d rsocb > >(tee stdout.log)
+
+if $(grep -v mail stdout.log | grep -q ERROR)
+then
+exit 1
+else
+exit 0
+fi
