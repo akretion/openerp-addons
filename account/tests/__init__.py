f1a756423d567d27af229039ddc44ca4a2ca7083
Holger Brunn <hbrunn@therp.nl>
2013-10-14 17:49:41 +0200

    [ADD] test for product_id_change

--- account/tests/__init__.py
+++ account/tests/__init__.py
@@ -1,4 +1,6 @@
-from . import test_tax
+from . import test_tax, test_product_id_change
 
-fast_suite = [test_tax,
+fast_suite = [
+        test_tax,
+        test_product_id_change,
               ]
