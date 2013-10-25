722bade2ec86f0ac5f8a669e1e5f26f8d7697fe7
Stefan Rijnhart <stefan@therp.nl>
2013-10-25 15:28:09 +0200

    [FIX] Due to the missing configuration for anglosaxon in the demo data,  tests from an unrelated module (purchase_double_validation) fails.  Removing the offensive check seems weak, but is warranted by the fact that  the check occurred in a backported improvement, which however did  contain the check itself. See  http://bazaar.launchpad.net/~openerp/openobject-addons/trunk/revision/8787.  Without the check, ocb-6.1 now contains the actual backported feature.

--- account_anglo_saxon/purchase.py
+++ account_anglo_saxon/purchase.py
@@ -32,9 +32,7 @@ class purchase_order(osv.osv):
             acc_id = order_line.product_id.property_stock_account_input and order_line.product_id.property_stock_account_input.id
             if not acc_id:
                 acc_id = order_line.product_id.categ_id.property_stock_account_input_categ and order_line.product_id.categ_id.property_stock_account_input_categ.id
-            if not acc_id:
-                raise osv.except_osv(_('Error !'), _('There is no stock input account defined for this product or category: "%s" (id:%d)') % (order_line.product_id.name, order_line.product_id.id,))    
-            else:    
+            if acc_id:
                 fpos = order_line.order_id.fiscal_position or False
                 account_id = self.pool.get('account.fiscal.position').map_account(cr, uid, fpos, acc_id)
         return account_id
