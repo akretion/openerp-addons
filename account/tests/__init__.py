ad545cbd8044f6a3fe07027db3df6374bf47cc87
Christophe CHAUVET <christophe.chauvet@syleam.fr>
2013-11-02 14:03:02 +0100

    [FIX] revert MP lp:754339

--- account/tests/__init__.py
+++ account/tests/__init__.py
@@ -1,6 +1,4 @@
-from . import test_tax, test_product_id_change
+from . import test_tax
 
-fast_suite = [
-        test_tax,
-        test_product_id_change,
+fast_suite = [test_tax,
               ]
